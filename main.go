c823d02ceeba1386fcc4539ca0bde3c36b94f7a1
donohutcheon@gmail.com <donohutcheon@gmail.com>
2020-04-11 21:09:09 +0200

    Fixing things

--- main.go
+++ main.go
@@ -15,27 +15,27 @@ import (
 )
 
 type Transaction struct {
-	DateTime time.Time `json:"dateTime" binding:"required" db:"dateTime"`
-	CentsAmount int64 `json:"centsAmount" binding:"required" db:"centsAmount"`
-	CurrencyCode string `json:"currencyCode" binding:"required" db:"currencyCode"`
+	DateTime time.Time `json:"dateTime" binding:"required" db:"datetime"`
+	CentsAmount int64 `json:"centsAmount" binding:"required" db:"cents_amount"`
+	CurrencyCode string `json:"currencyCode" binding:"required" db:"currency_code"`
 	Reference string `json:"reference" binding:"required" db:"reference"`
-	MerchantName string `json:"merchantName" binding:"required" db:"merchantName"`
-	MerchantCity string `json:"merchantCity" binding:"required" db:"merchantCity"`
-	MerchantCountryCode string `json:"merchantCountryCode" binding:"required" db:"merchantCountryCode"`
-	MerchantCountryName string `json:"merchantCountryName" binding:"required" db:"merchantCountryName"`
-	MerchantCategoryCode string `json:"merchantCategoryCode" binding:"required" db:"merchantCategoryCode"`
-	MerchantCategoryName string `json:"merchantCategoryName" binding:"required" db:"merchantCategoryName"`
+	MerchantName string `json:"merchantName" binding:"required" db:"merchant_name"`
+	MerchantCity string `json:"merchantCity" binding:"required" db:"merchant_city"`
+	MerchantCountryCode string `json:"merchantCountryCode" binding:"required" db:"merchant_country_code"`
+	MerchantCountryName string `json:"merchantCountryName" binding:"required" db:"merchant_country_name"`
+	MerchantCategoryCode string `json:"merchantCategoryCode" binding:"required" db:"merchant_category_code"`
+	MerchantCategoryName string `json:"merchantCategoryName" binding:"required" db:"merchant_category_name"`
 }
 
 
 func postTransactionHandler(db *sqlx.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS transactions (id SERIAL, dateTime timestamptz, 
-										centsAmount NUMERIC (16, 2), currencyCode VARCHAR NOT NULL, 
-										reference VARCHAR NOT NULL, merchantName VARCHAR NOT NULL,
-										merchantCity VARCHAR NOT NULL, merchantCountryCode VARCHAR NOT NULL,
-										merchantCountryName VARCHAR NOT NULL, merchantCategoryCode VARCHAR NOT NULL, 
-										merchantCategoryName VARCHAR NOT NULL)`); err != nil {
+		if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS transactions (id SERIAL, datetime timestamptz, 
+										cents_amount NUMERIC (16, 2), currency_code VARCHAR NOT NULL, 
+										reference VARCHAR NOT NULL, merchant_name VARCHAR NOT NULL,
+										merchant_city VARCHAR NOT NULL, merchant_country_code VARCHAR NOT NULL,
+										merchant_country_name VARCHAR NOT NULL, merchant_category_code VARCHAR NOT NULL, 
+										merchant_category_name VARCHAR NOT NULL)`); err != nil {
 			c.String(http.StatusInternalServerError,
 				fmt.Sprintf("Error creating database table: %q", err))
 			return
@@ -48,17 +48,17 @@ func postTransactionHandler(db *sqlx.DB) gin.HandlerFunc {
 		}
 
 		insertStatement := `INSERT INTO transactions
-				(dateTime, centsAmount, currencyCode, reference, merchantName,
-				merchantCity, merchantCountryCode, merchantCountryName, 
-				merchantCategoryCode, merchantCategoryName)
-				VALUES (:dateTime, :centsAmount, :currencyCode, :reference, :merchantName,
-				:merchantCity, :merchantCountryCode, :merchantCountryName, 
-				:merchantCategoryCode, :merchantCategoryName)`
+				(date_time, cents_amount, currency_code, reference, merchant_name,
+				merchant_city, merchant_country_code, merchant_country_name, 
+				merchant_category_code, merchant_category_name)
+				VALUES (:date_time, :cents_amount, :currency_code, :reference, :merchant_name,
+				:merchant_city, :merchant_country_code, :merchant_country_name, 
+				:merchant_category_code, :merchant_category_name)`
 		tx := db.MustBegin()
 		_, err := tx.NamedExec(insertStatement, transaction)
 		if err != nil {
 			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error incrementing tick: %q", err))
+				fmt.Sprintf("Error inserting transaction: %q", err))
 			return
 		}
 		tx.Commit()
@@ -68,9 +68,9 @@ func postTransactionHandler(db *sqlx.DB) gin.HandlerFunc {
 func getTransactionHandler(db *sqlx.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var transactions []Transaction
-		queryStatement := `SELECT dateTime, centsAmount, currencyCode, reference, merchantName,
-		merchantCity, merchantCountryCode, merchantCategoryName,
-			merchantCategoryCode, merchantCategoryName FROM transactions`
+		queryStatement := `SELECT date_time, cents_amount, currency_code, reference, merchant_name,
+		merchant_city, merchant_country_code, merchant_country_name,
+			merchant_category_code, merchant_category_name FROM transactions`
 		rows, err := db.Queryx(queryStatement)
 		if err != nil {
 			c.String(http.StatusInternalServerError,
